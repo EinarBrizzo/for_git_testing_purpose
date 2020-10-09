2d61ad8862c566fc6d06103d083286f9e316307a
Einar Brizzo <svolifeline@gmail.com>
2020-10-10 01:54:35 +0300

    Started writing calculator in app.js and created two functions which works. Alse created a new_print.py file with new f print functionality demonstration

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-alert('hello, dude!');
+alert('hello, dude!')
+
+function calculator(){
+    userChoice = (prompt("Enter 1 to start calculating or 0 to quit"))
+     while(userChoice !== 0) {
+        let firstNumber = parseFloat(prompt("Enter first number: "))
+        let secondNumber = parseFloat(prompt("Enter second number: "))
+        let operationSign = prompt("Enter the operation you want to do: ")
+
+        if (operationSign === "+") {
+            //alert("The result is: "+ addTwoNumbers (firstNumber, secondNumber))
+            alert(`The result is: ${addTwoNumbers (firstNumber, secondNumber)}`)
+        }
+        else if (operationSign === "-") {
+            //alert("The result is:" + " " + deductTwonumber(firstNumber, secondNumber))
+            alert(`The result is: ${deductTwonumber(firstNumber, secondNumber)}`)
+        }
+    }
+
+}
+
+function addTwoNumbers (x, y) {
+    return x + y
+}
+
+function deductTwonumber (x, y) {
+    return x - y
+}
+
+console.log(typeof firstNumber)
+
+calculator()
---
+++ new_print.py
@@ -0,0 +1,6 @@
+first_name = "Jack"
+last_name = "Jones"
+age = 34
+
+print(f"His name is {first_name}, and his age is {age}")
+print("His name is {0} and his last name is {1}".format(first_name, last_name))
